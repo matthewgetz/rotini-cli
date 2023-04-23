b78e62d85b8afef2a4270477a5bce06c8b43bedd
Matt Getz <matthewcgetz@gmail.com>
2023-04-23 14:30:22 -0700

    ver

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { rotini, } from 'rotini';
 import { definition, configuration, } from './cli';
 
 void (async (): Promise<void> => {
-  const program = rotini({ definition, configuration, });
-  const result = await program.run();
+  const result = await rotini({ definition, configuration, }).run();
   result?.handler_result && console.info(result.handler_result);
 })();
